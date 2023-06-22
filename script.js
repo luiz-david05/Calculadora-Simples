cdbf4d02a06a42207a748dc604fd4189d65af0a6
luiz-david05 <decd393@gmail.com>
2023-06-22 15:47:41 -0300

    add some features

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var display = document.getElementById("display");
     var buttons = document.querySelectorAll("button");
     var expression = "";
+    var easteregg = document.getElementById("easteregg");
     buttons.forEach(function (button) {
         button.addEventListener("click", function () {
             var buttonText = button.textContent;
             if (buttonText === "=") {
+                if (display.value === "") {
+                    return;
+                }
                 try {
                     var result = eval(expression);
                     display.value = "".concat(expression, " = ").concat(result);
@@ -24,10 +28,60 @@ document.addEventListener("DOMContentLoaded", function () {
                 display.value = display.value.slice(0, -1);
                 expression = expression.slice(0, -1);
             }
+            else if (buttonText === "x") {
+                display.value += "*";
+                expression += "*";
+            }
+            else if (buttonText === "÷") {
+                display.value += "/";
+                expression += "/";
+            }
+            else if (buttonText === "^") {
+                display.value += "**";
+                expression += "**";
+            }
             else {
                 display.value += buttonText;
                 expression += buttonText;
             }
+            if (display.value.length > 20) {
+                display.value = display.value.slice(0, 20);
+            }
+            if (expression === "3385") {
+                display.value = "Te amo, Camila!";
+                easteregg.textContent = "❤️";
+                expression = "";
+                setTimeout(function () {
+                    easteregg.textContent = "";
+                    display.value = "";
+                }, 5000);
+            }
         });
+    });
+    document.addEventListener("keydown", function (event) {
+        var key = event.key;
+        if (/[0-9]/.test(key)) {
+            display.value += key;
+            expression += key;
+        }
+        else if (key === "+" || key === "-" || key === "*" || key === "/") {
+            display.value += key;
+            expression += key;
+        }
+        else if (key === "Enter") {
+            try {
+                var result = eval(expression);
+                display.value = "".concat(expression, " = ").concat(result);
+                expression = "";
+            }
+            catch (error) {
+                display.value = "Error";
+                expression = "";
+            }
+        }
+        else if (key === "Backspace") {
+            display.value = display.value.slice(0, -1);
+            expression = expression.slice(0, -1);
+        }
     });
 });
--- script.ts
+++ script.ts
@@ -2,31 +2,82 @@ document.addEventListener("DOMContentLoaded", () => {
     const display = document.getElementById("display") as HTMLInputElement;
     const buttons = document.querySelectorAll("button");
     let expression = "";
-  
+    const easteregg = document.getElementById("easteregg") as HTMLInputElement; 
+
     buttons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const buttonText = button.textContent;
-  
-        if (buttonText === "=") {
-          try {
-            const result = eval(expression);
-            display.value = `${expression} = ${result}`;
-            expression = "";
-          } catch (error) {
-            display.value = "Error";
-            expression = "";
-          }
-        } else if (buttonText === "C") {
-          display.value = "";
-          expression = "";
-        } else if (buttonText === "CE") {
-          display.value = display.value.slice(0, -1);
-          expression = expression.slice(0, -1);
-        } else {
-          display.value += buttonText;
-          expression += buttonText;
-        }
-      });
+        button.addEventListener("click", () => {
+            const buttonText = button.textContent;
+
+            if (buttonText === "=") {
+                if (display.value === "") {
+                    return;
+                }
+                try {
+                    const result = eval(expression);
+                    display.value = `${expression} = ${result}`;
+                    expression = "";
+                } catch (error) {
+                    display.value = "Error";
+                    expression = "";
+                }
+            } else if (buttonText === "C") {
+                display.value = "";
+                expression = "";
+            } else if (buttonText === "CE") {
+                display.value = display.value.slice(0, -1);
+                expression = expression.slice(0, -1);
+            } else if (buttonText === "x") {
+                display.value += "*";
+                expression += "*";
+            } else if (buttonText === "÷") {
+                display.value += "/";
+                expression += "/";
+            } else if (buttonText === "^") {
+                display.value += "**";
+                expression += "**";
+            } else {
+                display.value += buttonText;
+                expression += buttonText;
+            }
+
+            if (display.value.length > 20) {
+                display.value = display.value.slice(0, 20);
+            }
+
+            if (expression === "3385"){
+                display.value = "Te amo, Camila!";
+                easteregg.textContent = "❤️";
+                expression = "";
+
+                setTimeout(() => {
+                    easteregg.textContent = "";
+                    display.value = "";
+                }
+                , 5000);
+            }
+        });
     });
-  });
-  
+    document.addEventListener("keydown", (event) => {
+        const key = event.key;
+
+        if (/[0-9]/.test(key)) {
+            display.value += key;
+            expression += key;
+        } else if (key === "+" || key === "-" || key === "*" || key === "/") {
+            display.value += key;
+            expression += key;
+        } else if (key === "Enter") {
+            try {
+                const result = eval(expression);
+                display.value = `${expression} = ${result}`;
+                expression = "";
+            } catch (error) {
+                display.value = "Error";
+                expression = "";
+            }
+        } else if (key === "Backspace") {
+            display.value = display.value.slice(0, -1);
+            expression = expression.slice(0, -1);
+        }
+    });
+});
